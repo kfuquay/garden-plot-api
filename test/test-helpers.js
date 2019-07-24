0543cb78559acd1eae66295bad0302ac9ae9bcd9
kfuquay <kirsty.fuquay@gmail.com>
2019-07-24 10:21:12 -0500

    add makeMaliciousPlot function to test-helpers

--- test/test-helpers.js
+++ test/test-helpers.js
@@ -42,65 +42,23 @@ function makePlotsArray() {
   ];
 }
 
-
-
-// const expectedProjects = [
-//   {
-//     id: 1,
-//     title: "First test project!",
-//     summary:
-//       "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus consequuntur deserunt commodi, nobis qui inventore corrupti iusto aliquid debitis unde non.Adipisci, pariatur.Molestiae, libero esse hic adipisci autem neque ?",
-//     user_id: 1,
-//     materials: ["one", "two"],
-//     steps: ["one", "two"],
-//     username: "dunder",
-//   },
-//   {
-//     id: 2,
-//     title: "second test project",
-//     summary: "zzzz",
-//     user_id: 1,
-//     materials: ["one", "two"],
-//     steps: ["one", "two"],
-//     username: "dunder",
-//   },
-//   {
-//     id: 3,
-//     title: "third test project!!",
-//     summary: "okokok",
-//     user_id: 2,
-//     materials: ["one", "two"],
-//     steps: ["one", "two"],
-//     username: "test",
-//   },
-//   {
-//     id: 4,
-//     title: "FOURTH TITLE",
-//     summary: "summmmaryy",
-//     user_id: 2,
-//     materials: ["one", "two"],
-//     steps: ["one", "two"],
-//     username: "test",
-//   },
-// ];
-
-// function makeMaliciousProject() {
-//   const maliciousProject = {
-//     id: 911,
-//     title: 'Naughty naughty very naughty <script>alert("xss");</script>',
-//     summary: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
-//   };
-//   const expectedProject = {
-//     ...maliciousProject,
-//     title:
-//       'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;',
-//     summary: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
-//   };
-//   return {
-//     maliciousProject,
-//     expectedProject,
-//   };
-// }
+function makeMaliciousPlot() {
+  const maliciousPlot = {
+    id: 911,
+    plotName: 'Naughty naughty very naughty <script>alert("xss");</script>',
+    plotNotes: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+  };
+  const expectedPlot = {
+    ...maliciousPlot,
+    title:
+      'Naughty naughty very naughty &lt;script&gt;alert("xss");&lt;/script&gt;',
+    summary: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`,
+  };
+  return {
+    maliciousProject,
+    expectedProject,
+  };
+}
 
 function makePlotsFixtures() {
   const testUsers = makeUsersArray();
@@ -143,4 +101,5 @@ module.exports = {
   cleanTables,
   makePlotsFixtures,
   makeUsersArray,
+  makeMaliciousPlot
 };
